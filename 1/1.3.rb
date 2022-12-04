857950b7897a1c1b1f2e7b520af6c2d47fa0e60c
ilia.kashtanov <ilia.kashtanov@t-systems.com>
2022-12-04 19:41:00 +0300

    fix task 3 & 4

--- 1/1.3.rb
+++ 1/1.3.rb
@@ -1,39 +1,32 @@
-puts "Первая сторона: "
+puts 'Первая сторона: '
 a = gets.chomp.to_i
 
-puts "Второая сторона: "
+puts 'Второая сторона: '
 b = gets.chomp.to_i
 
-puts "Третья сторона: "
+puts 'Третья сторона: '
 c = gets.chomp.to_i
 
 if a.zero? || b.zero? || c.zero?
-  puts "Введите все данные"
+  puts 'Введите все данные'
   return
 end
 
 if a == b && a == c
-  p "Треугольник равнобедренный и равносторонний"
+  p 'Треугольник равнобедренный и равносторонний'
   return
 end
 
-if a == b || a == c || b == c 
-  p "Треугольник равнобедренный"
+if a == b || a == c || b == c
+  p 'Треугольник равнобедренный'
   return
 end
 
-max_side = a > b ? a : b
-max_side = max_side > c ? a : c
+arr_sides = [a, b, c]
+max_side = arr_sides.max
+arr_sides.delete(max_side)
+is_rectangle = max_side**2 == arr_sides[0]**2 + arr_sides[1]**2
 
-is_rectangle = case max_side
-when a
-  a^2 == b^2 + c^2
-when b
-  b^2 == a^2 + c^2
-else
-  c^2 == a^2 + b^2
-end
+result = is_rectangle ? 'прямоугольный' : 'не прямоугольный'
 
-is_rectangle = is_rectangle ? "прямоугольный" : "не прямоугольный"
-
-p "Треуголник #{is_rectangle}"
+p "Треугольник #{result}"
--- 1/1.4.rb
+++ 1/1.4.rb
@@ -1,24 +1,23 @@
-puts "Первый коэфициент: "
+puts 'Первый коэфициент: '
 a = gets.chomp.to_i
 
-puts "Второй коэфициент: "
+puts 'Второй коэфициент: '
 b = gets.chomp.to_i
 
-puts "Третий коэфициент: "
+puts 'Третий коэфициент: '
 c = gets.chomp.to_i
 
-d = b^2-4*a*c
-result = ""
+d = b**2 - 4 * a * c
 
-if d > 0 
-  x1 = (-b + Math.sqrt(d)) / (2 * a)
-  x2 = (-b - Math.sqrt(d)) / (2 * a)
-  result = "Два корня, x1 = #{x1}, x2 = #{x2}"
-elsif (d == 0)
-  x = -b / (2*a)
-  result = "Один корень, x = #{x}"
-else
-  result = "Корней нет"
-end
+result = if d.positive?
+           x1 = (-b + Math.sqrt(d)) / (2 * a)
+           x2 = (-b - Math.sqrt(d)) / (2 * a)
+           "Два корня, x1 = #{x1}, x2 = #{x2}"
+         elsif d.zero?
+           x = -b / (2 * a)
+           "Один корень, x = #{x}"
+         else
+           'Корней нет'
+         end
 
 p result
